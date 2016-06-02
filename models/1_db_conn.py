67dd7e4f2326709f486f77e75c8bcf04365c7dcf
Carlos F. M. Faruolo <5carlosfelipe5@gmail.com>
2016-06-02 14:49:13 -0300

    Pegando o endereço do servidor do arquivo de configuração.
    Os endereços de servidor comuns devem ser compartilhados entre a equipe, mas fora do código.

--- models/1_db_conn.py
+++ models/1_db_conn.py
@@ -1,10 +1,8 @@
-#coding:utf-8
+# coding:utf-8
+
 from gluon import current
 from unirio.api import UNIRIOAPIRequest
-from unirio.api import APIServer
 
 # db = DAL('postgres://postgres:devdtic2@teste.sistemas.unirio.br/pesquisa', pool_size=10, driver_args={'connect_timeout': 5})
 
-api = UNIRIOAPIRequest(myconf.take('api.key'), APIServer.LOCAL, debug=False, cache=current.cache.ram)
-
-
+api = UNIRIOAPIRequest(myconf.take('api.key'), myconf.take('api.server'), debug=False, cache=current.cache.ram)
