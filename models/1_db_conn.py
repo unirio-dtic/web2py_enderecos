7bde35f6a5044ab75eca6e3a85be1fbba60783e4
Carlos F. M. Faruolo <5carlosfelipe5@gmail.com>
2016-07-15 16:05:09 -0300

    Removido configuracao residual

--- models/1_db_conn.py
+++ models/1_db_conn.py
@@ -3,6 +3,4 @@
 from gluon import current
 from unirio.api import UNIRIOAPIRequest
 
-# db = DAL('postgres://postgres:devdtic2@teste.sistemas.unirio.br/pesquisa', pool_size=10, driver_args={'connect_timeout': 5})
-
 api = UNIRIOAPIRequest(myconf.take('api.key'), myconf.take('api.server'), debug=False, cache=current.cache.ram)
